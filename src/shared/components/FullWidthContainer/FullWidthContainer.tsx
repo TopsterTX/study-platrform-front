fef60448f510bdc4415c300336ac68f4060ec1a5
TopsterTX <bbs.100@yandex.ru>
2024-06-22 16:09:30 +0300

    [feature] change package manager by the bun

--- src/shared/components/FullWidthContainer/FullWidthContainer.tsx
+++ src/shared/components/FullWidthContainer/FullWidthContainer.tsx
@@ -1,10 +1,10 @@
-import { Space } from "@/shared";
-import { ReactNode } from "react";
+import { Space } from '@/shared'
+import { ReactNode } from 'react'
 
 type FullWidthContainer = {
-  children: ReactNode;
-};
+  children: ReactNode
+}
 
 export const FullWidthContainer = ({ children }: FullWidthContainer) => {
-  return <article className="w-screen h-screen">{children}</article>;
-};
+  return <article>{children}</article>
+}
